65bf4d6ba1fdb72eaa6847c63106aedb41cab87f
= <=>
2024-01-14 10:46:05 +0530

    initial commit

--- 01-task-manager-api/app.js
+++ 01-task-manager-api/app.js
@@ -31,6 +31,7 @@ app.use(
     max: 100, // limit each IP to 100 requests per windowMs
   })
 );
+app.use(express.static('./public'));
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
